881b0b314d71ac6c03688a917571792024932d57
programacaogabriel <programacaogabriel@gmail.com>
2021-04-16 20:17:59 -0300

    Finalizado exercicio23

--- Vetores e Matrizes/src/Vetores23.java
+++ Vetores e Matrizes/src/Vetores23.java
@@ -5,53 +5,45 @@ public class Vetores23 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int count = 10;
-		float valor=0;
+		float valor = 0;
 		float vet[] = new float[count];
-		float vetX[] = new float[count];
-		float vetY[] = new float[count];
+		float vetX[] = new float[5];
+		float vetY[] = new float[5];
 		float vetF[] = new float[count];
-		
+
 		System.out.println("Preencha os vetores");
-	
-		for(int i=0;i<vet.length;i++) {
-			vet[i]=sc.nextFloat();
+
+		for (int i = 0; i <= 4; i++) {
+			vetX[i] = sc.nextFloat();
 		}
-		for(int i=0; i<vet.length;i++) {
-			if(i<=4) {
-				vetX[i]=vet[i];
-			}if(i>=5) {
-				vetY[i]=vet[i];
-			}
-			
+		for (int i = 0; i <= 4; i++) {
+			vetY[i] = sc.nextFloat();
 		}
-		
+
 		System.out.println();
 		System.out.println("valores de Y");
-		for(int i=0;i<vet.length;i++) {
-		if(vetY[i]!=0) {
-			System.out.print(vetY[i]+" ");
-		}
-			
+		for (int i = 0; i < 5; i++) {
+			if (vetY[i] != 0) {
+
+				System.out.print(vetY[i] + " ");
+			}
+
 		}
 		System.out.println();
 		System.out.println("Valores de X");
-		for(int i=0;i<vet.length;i++) {
-			if(vetX[i]!=0) {
-			System.out.print(vetX[i]+" ");
+		for (int i = 0; i < 5; i++) {
+			if (vetX[i] != 0) {
+				System.out.print(vetX[i] + " ");
 			}
 		}
-		
-		for(int i=0; i<vet.length;i++) {
-			for(int j =0;j<vet.length;j++) {
-				vetF[j]=vetX[i]*vetY[i];
-				System.out.print(vetF[j]+" ");
-			}
+		System.out.println();
+		System.out.println("Valores ");
+		for (int i = 0; i < 5; i++) {
+			vetF[i] = vetX[i] * vetY[i];
+			System.out.print(vetF[i] +" ");
 			
 		}
-		
-		
-		
-	sc.close();
-	}
 
+		sc.close();
+	}
 }
